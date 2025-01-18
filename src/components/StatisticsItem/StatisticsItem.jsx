f8541ddf501bf2393683797ddcf02073c45f0f2e
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-01-18 15:53:44 +0100

    4 tasks

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,16 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import { IconContext } from 'react-icons';
+import style from './StatisticsItem.module.css';
+
+const StatisticsItem = ({ title, total, icon }) => {
+  return (
+    <>
+      <IconContext.Provider value={{ color: 'black', size: 30 }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
